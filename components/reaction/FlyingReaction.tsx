120437534ff1579e37046fb2ec8d7b45744ac9ce
ilkinsufi <ilkinsufi@gmail.com>
2024-02-08 11:55:44 +0400

    Flying Reaction add

---
+++ components/reaction/FlyingReaction.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FlyingReaction = () => {
+  return <div></div>;
+};
+
+export default FlyingReaction;
---
+++ components/reaction/ReactionButton.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReactionButton = () => {
+  return <div></div>;
+};
+
+export default ReactionButton;
